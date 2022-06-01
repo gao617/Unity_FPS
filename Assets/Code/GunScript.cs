b5666ef1270a0eb060b118d7b901f20dfc7cb6be
Vincent Lin <lin971@purdue.edu>
2022-06-01 02:11:30 -0400

    finish project

---
+++ Assets/Code/GunScript.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    // float rotationY = 0F;
+    // float rotationX = 0F;
+
+    public Camera fpsCam;
+    public GameObject bullet;
+    public float damage = 10f;
+    public int count;
+    public bool gunTriggered; 
+
+    private void Start()
+    {
+        count = 0;
+    }
+
+    void Update ()
+    {
+        // if (axes == RotationAxes.MouseXAndY)
+        // {
+        //     rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        //     rotationX = Mathf.Clamp(rotationX, -40, 40);
+        //
+        //     rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        //     rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+        //
+        //     transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        // }
+        // else if (axes == RotationAxes.MouseX)
+        // {
+        //     transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        // }
+        // else
+        // {
+        //     rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        //     rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+        //
+        //     transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        // }
+
+        //Ray ray = fpsCam.ScreenPointToRay(Input.mousePosition);
+        var gunTransform = GameObject.Find("GunPrefab").GetComponent<Transform>(); //get the transform of gun prefab
+        Ray ray = new Ray(gunTransform.position, gunTransform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            // Debug.Log("Object: " + hit.collider.name);
+            // Debug.DrawLine(ray.origin, hit.point, Color.green, 2, false);
+            // if (Input.GetButtonDown("Fire1"))
+            // {
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (gunTriggered == true)
+            {
+                Debug.Log("gun is triggered!!!!!!!!!!!!!!!!!!!!!!!");
+                GameObject gb = Instantiate(bullet, gunTransform.position, gunTransform.rotation);
+                count = 0;
+                Rigidbody rb = gb.GetComponent<Rigidbody>();
+                rb.AddForce(ray.direction * 3000f);
+                Destroy(gb, 0.5f);
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+
+                // count = 0;
+            }
+            //
+            // count++;
+            //
+
+
+            // }
+        }
+
+    }
+
+    //void showObject()
+}
--- Assets/Code/Wrapper.cs
+++ Assets/Code/Wrapper.cs
@@ -7,7 +7,7 @@ public class Wrapper : MonoBehaviour
 {
     public struct AerowandData
     {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)] //used to create array in C# class
         public float[] position; //3
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
         public float[] orientation; //4
@@ -28,57 +28,115 @@ public class Wrapper : MonoBehaviour
     public static extern AerowandData GetAerowandData(int deviceId);
     [DllImport("aerowand", EntryPoint ="TriggerHaptics")]
     public static extern void TriggerHaptics(int deviceId,  byte effect);
-    [DllImport("aerowand", EntryPoint ="PrintAerowandData")]
-    public static extern void PrintAerowandData(AerowandData aerowandData);
-
-    public int[] devices;
-    public Transform[] children;
+    
+    
+    private int[] devices; //array of devices, including left and right controllers (0 and 1)
+    private Transform[] children; //the array of transform (pos, rot) of all children under this Game object (camera)
+    private Quaternion rotationOffset; //the offset used to 
+    public bool shieldHit = false; //true when the shield is hit by enemy's bullets
     
     void Start()
     {
         byte numDevices = InitializeDevices();
         Debug.Log("numDevices is" + numDevices);
 
-        devices = new int[]{GetAerowandHand1(), GetAerowandHand2()};
+        devices = new int[]{GetAerowandHand1(), GetAerowandHand2()}; //get left and right controllers
         Debug.Log("devices are" + devices[0] + ", "+ devices[1]);
 
-      
+        rotationOffset = Quaternion.identity; //initialize to be (1,0i,0j,0k) (no rotation)
+        // tempQuaternion = (new float[] {0, 0, 0, 0});
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        children = GetComponentsInChildren<Transform>();
+        children = GetComponentsInChildren<Transform>(); //get all the transform of all children, e.g. gun is the first child under camera, so it's children[1] and shield is the 9th children under camera
         
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~For shield~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        var shieldData = GetAerowandData(devices[0]);
-        Debug.Log("x:" + shieldData.position[0] + " y:" + shieldData.position[1] + " z:" + shieldData.position[2] + "  rw:" + shieldData.orientation[0] + " rx:" + shieldData.orientation[1] + " ry:" + shieldData.orientation[2] + " rz:" + shieldData.orientation[3] );
-        // PrintAerowandData(data);
-        children[9].localPosition = new Vector3(-shieldData.position[0] * (float)1.5, shieldData.position[1] - (float)0.6, shieldData.position[2] + (float)1.5);
-        // GetComponent<Transform>().rotation = new Quaternion(data.orientation[0], data.orientation[1],
-        //     data.orientation[2], data.orientation[3]);
-        var euler = new Quaternion(shieldData.orientation[0], shieldData.orientation[1], shieldData.orientation[2], shieldData.orientation[3]).eulerAngles;
-        children[9].rotation = Quaternion.Euler(-euler[1] + 90, euler[0] + 20, -euler[2]);
-        if (shieldData.trigger > 0)
+        //get data from devices
+        var shieldData = GetAerowandData(devices[0]); 
+        var gunData = GetAerowandData(devices[1]);
+        
+        //reset the rotationOffset if trigger button are pressed on both controllers 
+        if (shieldData.trigger > 0 && gunData.trigger > 0)
         {
-            TriggerHaptics(devices[0], 1);
+            //reset rotation & position
+            Debug.Log("resetting rotation offset!");
+            Quaternion targetQuaternion = new Quaternion( 1, 0, 0, 0);
+            Quaternion originalQuaternion = new Quaternion(shieldData.orientation[0], -shieldData.orientation[1], shieldData.orientation[3], -shieldData.orientation[2]);
+            rotationOffset = targetQuaternion * Quaternion.Inverse(originalQuaternion);
+            return; 
         }
-        if (shieldData.face > 0) {
-            //std::cout << i << " face" << std::endl;
-        }
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~For shield~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+         // Debug.Log("  rw:" + shieldData.orientation[0] + " rx:" + shieldData.orientation[1] + " ry:" + shieldData.orientation[2] + " rz:" + shieldData.orientation[3] );
+         // PrintAerowandData(data);
+         children[9].localPosition = new Vector3(-shieldData.position[0] * (float)1.5, -shieldData.position[1] * (float)2, shieldData.position[2] + (float)1.5); //scaled to make the position "1 to 1" in the game scene
+         // GetComponent<Transform>().rotation = new Quaternion(data.orientation[0], data.orientation[1],
+         //     data.orientation[2], data.orientation[3]);
+         // var eulerShiled = new Quaternion(shieldData.orientation[0], shieldData.orientation[1], shieldData.orientation[2], shieldData.orientation[3]).eulerAngles;
+         // children[9].rotation = Quaternion.Euler(-eulerShiled[1] + 90, eulerShiled[0] + 20, -eulerShiled[2]);
+
+         // children[9].rotation = new Quaternion(shieldData.orientation[0], shieldData.orientation[1], shieldData.orientation[2], shieldData.orientation[3]);
+         // children[9].rotation = children[1].rotation * new Quaternion((float)0.7071, 0, (float)-0.7071, 0);
+         children[9].rotation = rotationOffset * (new Quaternion(shieldData.orientation[0], -shieldData.orientation[1],
+         shieldData.orientation[3], -shieldData.orientation[2]));
+         // children[9].rotation = new Quaternion(shieldData.orientation[2], -shieldData.orientation[1],
+         //     shieldData.orientation[3], -shieldData.orientation[0]);
+         // var rotation = new Quaternion(shieldData.orientation[0], -shieldData.orientation[1],
+         // shieldData.orientation[3], -shieldData.orientation[2]);
+         // rotation.ToAngleAxis();
+         
+         if (shieldHit == true)
+         {
+             TriggerHaptics(devices[0], 1);
+             shieldHit = false; //reset value
+             // Debug.Log(" Shield is hit!!!!!!!!");
+         }
+         // if (shieldData.face > 0) {
+         //     //std::cout << i << " face" << std::endl;
+         // }
+        
+        // var shieldData = GetAerowandData(devices[0]);
+        // Debug.Log("x:" + shieldData.position[0] + " y:" + shieldData.position[1] + " z:" + shieldData.position[2] + "  rw:" + shieldData.orientation[0] + " rx:" + shieldData.orientation[1] + " ry:" + shieldData.orientation[2] + " rz:" + shieldData.orientation[3] );
+        // // PrintAerowandData(data);
+        // children[9].localPosition = new Vector3(shieldData.position[0], shieldData.position[1], shieldData.position[2]);
+        // // GetComponent<Transform>().rotation = new Quaternion(data.orientation[0], data.orientation[1],
+        // //     data.orientation[2], data.orientation[3]);
+        // var euler = new Quaternion(shieldData.orientation[0], shieldData.orientation[1], shieldData.orientation[2], shieldData.orientation[3]).eulerAngles;
+        // children[9].rotation = Quaternion.Euler(euler[1], euler[0], euler[2]);
+        // if (shieldData.trigger > 0)
+        // {
+        //     TriggerHaptics(devices[0], 1);
+        // }
+        // if (shieldData.face > 0) {
+        //     //std::cout << i << " face" << std::endl;
+        // }
         
         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~For gun~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        var gunData = GetAerowandData(devices[1]);
-        Debug.Log("x:" + gunData.position[0] + " y:" + gunData.position[1] + " z:" + gunData.position[2] + "  rw:" + gunData.orientation[0] + " rx:" + gunData.orientation[1] + " ry:" + gunData.orientation[2] + " rz:" + gunData.orientation[3] );
+
+        // Debug.Log("x:" + gunData.position[0] + " y:" + gunData.position[1] + " z:" + gunData.position[2] + "  rw:" + gunData.orientation[0] + " rx:" + gunData.orientation[1] + " ry:" + gunData.orientation[2] + " rz:" + gunData.orientation[3] );
         // PrintAerowandData(data);
-        children[2].localPosition = new Vector3(-gunData.position[0] * (float)1.5, gunData.position[1] - (float)0.6, gunData.position[2] + (float)1.5);
+        children[1].localPosition = new Vector3(-gunData.position[0] * (float)1.5, -gunData.position[1] * (float)2, gunData.position[2] + (float)1.5);
         // GetComponent<Transform>().rotation = new Quaternion(data.orientation[0], data.orientation[1],
         //     data.orientation[2], data.orientation[3]);
-        euler = new Quaternion(gunData.orientation[0], gunData.orientation[1], gunData.orientation[2], gunData.orientation[3]).eulerAngles;
-        children[2].rotation = Quaternion.Euler(-euler[1] + 90, euler[0] + 20, -euler[2]);
+        // var euler = new Quaternion(gunData.orientation[0], -gunData.orientation[1], gunData.orientation[3], gunData.orientation[2]).eulerAngles;
+        // children[1].rotation = Quaternion.Euler(-euler[1] + 90, euler[0] + 20, -euler[2]);
+
+        children[1].rotation = rotationOffset * (new Quaternion(gunData.orientation[0], -gunData.orientation[1], gunData.orientation[3], -gunData.orientation[2]));
+        
         if (gunData.trigger > 0)
         {
             TriggerHaptics(devices[1], 1);
+            GunScript gunScript = GameObject.Find("Gun").GetComponent<GunScript>(); //get the script from the gun object
+            gunScript.gunTriggered = true; //set trigger to true and shoot bullet
+            Debug.Log("gun triggered!!!!!!!!");
+        }
+        else
+        {
+            GunScript gunScript = GameObject.Find("Gun").GetComponent<GunScript>(); //get the script from the gun object
+            gunScript.gunTriggered = false; //set trigger to true and shoot bullet
         }
         if (gunData.face > 0) {
             //std::cout << i << " face" << std::endl;
